151136aba1fcb56a5d5bc9436969e1ecef3c1f36
harish sivakumar <54032669+Harish-Sivakumar@users.noreply.github.com>
2019-08-16 22:18:11 +0530

    Update prime num in an intrval.py

--- add of 2 num/prime num in an intrval.py
+++ add of 2 num/prime num in an intrval.py
@@ -1,6 +1,8 @@
-start=int(input("enter 1st num"))
-stop=int(input("enter 2nt num"))
-for i in range(start,stop+1):
+a=input()
+b=a.split(" ")
+c=b[0]
+d=b[1]
+for i in range(int(c)+1,int(d)):
    # if i>1:
 
        for j in range(2,i):
